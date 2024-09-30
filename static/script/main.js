39bec191ecc79f469cf315fe33366e7fc980f678
Alona <alona.sorochynska@gmail.com>
2024-10-01 00:02:47 +0100

    create navbar for small devices

--- static/script/main.js
+++ static/script/main.js
@@ -19,12 +19,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstBlock = document.querySelector(
         ".main-container-main-page .main-container:first-of-type"
     );
-
     if (firstBlock) {
         const h3Name = firstBlock.querySelector('.h3-name');
         if (h3Name) {
             h3Name.classList.add("rainbow-text");
         }
     }
-});
 
+    const menuIcon = document.getElementById('menu-icon');
+    const navbarItems = document.querySelectorAll('.navbar-item');
+    const currentUrl = window.location.href;
+
+    navbarItems.forEach(item => {
+        if (item.href === currentUrl) {
+            menuIcon.textContent = item.textContent;
+        }
+    });
+});
