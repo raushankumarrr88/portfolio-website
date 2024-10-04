24af73098cfeeb68f2709f13b5fbd04a57a999d2
Alona <alona.sorochynska@gmail.com>
2024-10-04 19:58:59 +0100

    edit menu for small device

--- static/script/script.js
+++ static/script/script.js
@@ -1,18 +1,29 @@
-let menu = document.querySelector('#menu-icon-js');
-let menuicon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let navtc = document.querySelector('#nav-tc-js');
+document.addEventListener("DOMContentLoaded", function() {
+  const menuElement = document.getElementById("js-menu");
+  const menuIcon = menuElement.querySelector(".burger-menu");
+  const menuText = menuElement.querySelector(".menu-text");
+  const closeButton = menuElement.querySelector(".menu-close-btn");
+  const navbar = document.querySelector(".navbar");
 
-menu.onclick = () => {
-	menuicon.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-	navtc.classList.toggle("nav-touch-close-open");
-}
+  menuText.addEventListener("click", function() {
+    menuElement.classList.add("menu-opened");
+    navbar.classList.add("open");
+    menuIcon.style.display = "none";
+    closeButton.style.display = "inline-block";
+  });
 
-navtc.onclick = () => {
-	menuicon.classList.toggle('bx-x');
-	navbar.classList.remove('open');
-	navtc.classList.remove('nav-touch-close-open');
-	navtc.classList.remove("nav-tc-z");
-	navtc.classList.remove("nav-LR-TC");
-}
+  closeButton.addEventListener("click", function() {
+    menuElement.classList.remove("menu-opened");
+    navbar.classList.remove("open");
+    closeButton.style.display = "none";
+    menuIcon.style.display = "inline-block";
+  });
+
+  const navbarItems = document.querySelectorAll('.navbar-item');
+  const currentUrl = window.location.href;
+  navbarItems.forEach(item => {
+    if (item.href === currentUrl) {
+      menuText.textContent = item.textContent;
+    }
+  });
+});
